736f786ac214fa5c41ca0e412a13a11fa12c0b57
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-10-02 21:47:51 +0900

    🎨 추천 페이지 수정

--- src/pages/result/result.jsx
+++ src/pages/result/result.jsx
@@ -6,9 +6,10 @@ function App() {
     return (
       <div className={styles["app-container"]}>
        <div className={styles["top-container"]}>
-
+        <p className={styles["top-comment"]}>
+        서현님께<br />"넷플릭스"를 추천합니다!
+        </p>
        </div>
-
       </div>
     );
   }
